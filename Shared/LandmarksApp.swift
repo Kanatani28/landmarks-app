abcfce9b397a45ce16f5e3d9b3852cbea215dcb5
maroKanatani <16130443+Kanatani28@users.noreply.github.com>
2021-12-31 12:45:56 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 金谷政大 on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
